e53c5acaa0dd39169d41037cc72848df124d2b4d
whitememories0317 <55526002+whitememories0317@users.noreply.github.com>
2019-10-09 15:19:46 +0800

    Complete Project

--- src/GameLogic.cs
+++ src/GameLogic.cs
@@ -5,28 +5,29 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
+using SwinGameSDK;
 static class GameLogic
 {
-	public static void Main()
+	public static void Main ()
 	{
 		//Opens a new Graphics Window
-		SwinGame.OpenGraphicsWindow("Battle Ships", 800, 600);
+		SwinGame.OpenGraphicsWindow ("Battle Ships", 800, 600);
 
 		//Load Resources
-		LoadResources();
+		GameResources.LoadResources ();
 
-		SwinGame.PlayMusic(GameMusic("Background"));
+		SwinGame.PlayMusic (GameResources.GameMusic ("Background"));
 
 		//Game Loop
 		do {
-			HandleUserInput();
-			DrawScreen();
-		} while (!(SwinGame.WindowCloseRequested() == true | CurrentState == GameState.Quitting));
+			GameController.HandleUserInput ();
+			GameController.DrawScreen ();
+		} while (!(SwinGame.WindowCloseRequested () == true | GameController.CurrentState == GameState.Quitting));
 
-		SwinGame.StopMusic();
+		SwinGame.StopMusic ();
 
 		//Free Resources and Close Audio, to end the program.
-		FreeResources();
+		GameResources.FreeResources ();
 	}
 }
 
